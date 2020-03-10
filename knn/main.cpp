43f9be9cee872b9242016db2e5de2e55b8b9c3ba
Emmanuel <emgg90@gmail.com>
2020-03-09 23:15:48 -0700

    Read cancer date from CSV

--- knn/main.cpp
+++ knn/main.cpp
@@ -1,36 +1,88 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 
-class Point
+class DataSet
 {
 public:
-    std::vector<std::vector<double>> features;
-
     std::vector<int> class_val;
+    std::vector<std::vector<double>> features;
 };
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
 
-    Point point;
-
-    point.features = {{1, 2, 3}};
-
-    std::cout << point.features[0][1] << std::endl;
+    DataSet data;
 
     // Read data
     std::ifstream file("../data/cancer/wdbc.data");
+    std::string line_values;
 
-    std::string value;
+    // Set the number of rows
+    /* 
+        Very careful here, if we put do the resize of class_val after features,
+        because we will resize features we get problems afterwards
+    */
+    data.class_val.resize(569);
+    data.features.resize(569);
 
+    // std::cout << data.class_val[0] << std::endl;
+    int row = -1;
     while (file.good())
     {
-        std::getline(file, value, ',');
-        if (value.compare("\n")==0)
-            std::cout << 'a' << std::endl;
-        std::cout << value << std::endl;
+        row++;
+
+        // Resize the columns
+        data.features[row].resize(32 - 2);
+
+        // Read the entire line
+        std::getline(file, line_values, '\n');
+
+        // Now we read each value (constructor of stringstream)
+        std::stringstream ss = std::stringstream(line_values);
+
+        std::string line_value;
+
+        int col = 0;
+        while (std::getline(ss, line_value, ','))
+        {
+            // std::cout << line_value << std::endl;
+
+            if (col == 1)
+            {
+                if (line_value.compare("M") == 0)
+                {
+                    data.class_val[row] = 1;
+                }
+                else
+                {
+                    data.class_val[row] = 0;
+                }
+            }
+
+            if (col > 1)
+            {
+                // Convert to double
+                data.features[row][col - 2] = std::stod(line_value);
+            }
+            col++;
+        }
+        std::cout << row + 1 << " " << data.class_val[row] << std::endl;
+    }
+
+    for (int row = 0; row < data.features.size(); row++)
+    {
+        for (int col = 0; col < data.features[row].size(); col++)
+        {
+            std::cout << data.features[row][col] << ' ' << std::scientific;
+        }
+        std::cout << std::endl;
+    }
+
+    for (int row = 0; row < data.class_val.size(); row++)
+    {
+        std::cout << row + 1 << " " << data.class_val[row] << std::endl;
     }
 
     return 0;
